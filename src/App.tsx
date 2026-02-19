a8c5b4713bbb1f07b39e6de93bf9950c2cf308b4
Felipe-Yasuo <felipe.yasuo@hotmail.com>
2026-02-18 22:18:10 -0300

    chore: setup vite react ts and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-900 text-white">
+      <h1 className="text-4xl font-bold text-blue-400">
+        Testando
+      </h1>
+    </div>
+  );
+}
+
